572bbb4e3f641b7521aeabf208d88a2aefe50582
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-10-26 13:05:13 +0300

    create initial routers

---
+++ client/src/components/BuilderPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function BuilderPage() {
+  return <div>form builder page</div>;
+}
+
+export default BuilderPage;
---
+++ client/src/components/SubmitPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SubmitPage() {
+  return <div>form submit page</div>;
+}
+
+export default SubmitPage;
