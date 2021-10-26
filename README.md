30a1097b08035f4ae0497a2a168ab4dd06f37294
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-10-26 13:35:27 +0300

    create initial list page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# huckerU-solution-task
+# hackerU-solution-task
+
+[link to diagram](https://drawsql.app/cyber4s-16/diagrams/myform#)
--- client/src/components/ListPage.js
+++ client/src/components/ListPage.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const fakeData = [
+  {
+    id: 1,
+    formName: 'Car campaign',
+    Submissions: 15,
+  },
+  {
+    id: 2,
+    formName: 'Job Application',
+    Submissions: 20,
+  },
+  {
+    id: 3,
+    formName: 'School Exam',
+    Submissions: 12,
+  },
+];
 
 function ListPage() {
-  return <div>list page</div>;
+  const [formList, setFormList] = useState([]);
+
+  useEffect(() => {
+    setFormList(fakeData);
+  }, []);
+
+  return (
+    <div>
+      <h1>Form list</h1>
+      <table>
+        <tbody>
+          <tr>
+            <th>ID</th>
+            <th>Form name</th>
+            <th>Submissions</th>
+            <th>Submit Page</th>
+          </tr>
+          {formList?.map((form, i) => {
+            return (
+              <tr key={i}>
+                <td>{form.id}</td>
+                <td>{form.formName}</td>
+                <td>{form.Submissions}</td>
+                <td>
+                  <Link to={`/${form.id}`}>view submit</Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default ListPage;
