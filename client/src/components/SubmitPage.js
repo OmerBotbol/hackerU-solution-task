87a6b34e447650e590351c49ee84ee785b40f636
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-10-26 17:48:06 +0300

    create initial submit page

--- client/src/components/SubmitPage.js
+++ client/src/components/SubmitPage.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router';
+
+const fakeQuestions = [
+  { id: 1, label: 'what is your name?', type: 'text' },
+  { id: 2, label: 'how old are you?', type: 'number' },
+  { id: 3, label: 'what is your email?', type: 'email' },
+];
+
+const fakeTitle = 'cool form';
 
 function SubmitPage() {
-  return <div>form submit page</div>;
+  const [questions, setQuestions] = useState([]);
+  const [formName, setFormName] = useState('');
+  const [answers, setAnswers] = useState([]);
+  const [isFinished, setIsFinished] = useState(false);
+
+  useEffect(() => {
+    /** http request to get questions and the form name by form id */
+    setTimeout(() => {
+      setQuestions(fakeQuestions);
+      setFormName(fakeTitle);
+    }, 1000);
+  }, []);
+
+  const handleChange = (answer, questionId, i) => {
+    const answerArr = [...answers];
+    answerArr[i] = { questionId, answer };
+    setAnswers(answerArr);
+  };
+
+  const handleSubmit = () => {
+    if (answers.length === questions.length) {
+      /** http request to post the answers to the server */
+      console.log(answers);
+      return setIsFinished(true);
+    }
+    console.log('missing answers');
+  };
+
+  if (questions.length === 0 || !formName) {
+    return <div>LOADING...</div>;
+  }
+
+  if (isFinished) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div>
+      <h1>{formName}</h1>
+      {questions.map((question, i) => {
+        return (
+          <div key={i}>
+            <label>{question.label}</label>
+            <input
+              type={question.type}
+              onChange={(e) => handleChange(e.target.value, question.id, i)}
+            />
+          </div>
+        );
+      })}
+      <button onClick={() => handleSubmit()}>SUBMIT</button>
+    </div>
+  );
 }
 
 export default SubmitPage;
