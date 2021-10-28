11ffd3c52dfd53e560355ddc88af5fc4720bd7f5
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-10-28 17:06:49 +0300

    add loading spinner

--- resources/js/components/ListPage.js
+++ resources/js/components/ListPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Loader from "react-loader-spinner";
 import { Link } from "react-router-dom";
 import { Redirect } from "react-router";
 import axios from "axios";
@@ -22,37 +23,48 @@ function ListPage() {
         <div id="list-page-container">
             <h1 id="list-page-header">Form list</h1>
             <div id="table-container">
-                <table id="list-table">
-                    <tbody>
-                        <tr>
-                            <th className="table-header">ID</th>
-                            <th className="table-header">Form name</th>
-                            <th className="table-header">Submissions</th>
-                            <th className="table-header">Submit Page</th>
-                        </tr>
-                        {formList?.map((form, i) => {
-                            return (
-                                <tr key={i}>
-                                    <td className="table-line">{form.id}</td>
-                                    <td className="table-line">
-                                        {form.formName}
-                                    </td>
-                                    <td className="table-line">
-                                        {form.submissions}
-                                    </td>
-                                    <td className="table-line">
-                                        <Link
-                                            className="submit-link"
-                                            to={`/${form.id}`}
-                                        >
-                                            view submit
-                                        </Link>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
+                {formList.length === 0 ? (
+                    <Loader
+                        type="Oval"
+                        color="#77a6f7"
+                        height={100}
+                        width={100}
+                    />
+                ) : (
+                    <table id="list-table">
+                        <tbody>
+                            <tr>
+                                <th className="table-header">ID</th>
+                                <th className="table-header">Form name</th>
+                                <th className="table-header">Submissions</th>
+                                <th className="table-header">Submit Page</th>
+                            </tr>
+                            {formList?.map((form, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td className="table-line">
+                                            {form.id}
+                                        </td>
+                                        <td className="table-line">
+                                            {form.formName}
+                                        </td>
+                                        <td className="table-line">
+                                            {form.submissions}
+                                        </td>
+                                        <td className="table-line">
+                                            <Link
+                                                className="submit-link"
+                                                to={`/${form.id}`}
+                                            >
+                                                view submit
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                )}
                 <div
                     className="custom-button"
                     onClick={() => setIsRedirect(true)}
